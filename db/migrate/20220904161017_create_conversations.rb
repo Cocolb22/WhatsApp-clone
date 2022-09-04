432ba39a6c6f84864e157ed56ae3bef3f7a11519
Corentin Le Bris <corentin.lebris@hotmail.fr>
2022-09-04 18:45:07 +0200

    create conversation model, add message ref to conversation

---
+++ db/migrate/20220904161017_create_conversations.rb
@@ -0,0 +1,9 @@
+class CreateConversations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :conversations do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null:false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_04_154711) do
+ActiveRecord::Schema.define(version: 2022_09_04_163654) do
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "message_id", null: false
+    t.index ["message_id"], name: "index_conversations_on_message_id"
+    t.index ["user_id"], name: "index_conversations_on_user_id"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text "content", null: false
@@ -35,5 +45,7 @@ ActiveRecord::Schema.define(version: 2022_09_04_154711) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "conversations", "messages"
+  add_foreign_key "conversations", "users"
   add_foreign_key "messages", "users"
 end
